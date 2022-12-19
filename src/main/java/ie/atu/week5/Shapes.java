19974d4fc3255402af8804191ba36e86b20378a2
Olamide Aderogba <G00401971@atu.ie>
2022-12-19 10:38:50 +0000

    Prompt user for length

--- src/main/java/ie/atu/week5/Shapes.java
+++ src/main/java/ie/atu/week5/Shapes.java
@@ -1,4 +1,11 @@
 package ie.atu.week5;
+import java.util.Scanner;
 
 public class Shapes {
+    public static void main(String[] args) {
+        System.out.println("Please enter width ofthe rectangle");
+        Scanner input = new Scanner(System.in);
+        double length- input.nextDouble();
+        System.out.println("You entered" + length);
+    }
 }
