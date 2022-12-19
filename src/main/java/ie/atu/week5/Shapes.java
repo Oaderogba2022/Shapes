71a1bfb8e03abb533effb3f8caea714de6e5cfca
Olamide Aderogba <G00401971@atu.ie>
2022-12-19 10:41:41 +0000

    Prompt user for Width

--- src/main/java/ie/atu/week5/Shapes.java
+++ src/main/java/ie/atu/week5/Shapes.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 
 public class Shapes {
     public static void main(String[] args) {
-        System.out.println("Please enter width ofthe rectangle");
+        System.out.println("Please enter length of the rectangle");
         Scanner input = new Scanner(System.in);
         double length- input.nextDouble();
         System.out.println("You entered" + length);
+
+        System.out.println("Please enter width of the rectangle");
+        double width- input.nextDouble();
+        System.out.println("You entered" + width);
     }
 }
